146ad8bc4c57bcde68d8af9fc4421e8e79e2d0e8
moraloyuxing <s110319023@stu.ntue.edu.tw>
2017-02-23 18:47:37 +0800

    Update B3.js

--- B3.js
+++ B3.js
@@ -9,3 +9,11 @@ var greetMe = function() {
 	console.log('Hi Tony!');
 }
 greetMe();
+
+//arrow function expression
+greetID = () => console.log(`Hi Yuxing!`);
+greetID();
+
+//arrow function expression with parameters
+greetFirst = name =>console.log(`Hi ${name} !`);
+greetFirst(`Lin`);
